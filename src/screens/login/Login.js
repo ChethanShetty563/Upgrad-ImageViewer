13876b73e377e54f013bda8897f54e2fba8e5723
ChethanShetty563 <chethan563@gmail.com>
2021-03-15 21:56:02 +0530

    Added the card for login form

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import "./Login.css";
+import Header from "../../common/header/Header";
+import Card from "@material-ui/core/Card";
+import FormControl from "@material-ui/core/FormControl";
+import Typography from "@material-ui/core/Typography";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Button from "@material-ui/core/Button";
+import FormHelperText from "@material-ui/core/FormHelperText";  
 
 class Login extends Component {
 
     render() {
         return (
           <div>
+              <div className="card-container">
+          <Card>
+
+          </Card>
+          </div>
     </div>
     );
   }
