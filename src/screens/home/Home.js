51784652fbbf8cee1c6ec8582ebd144d2ebdea13
ChethanShetty563 <chethan563@gmail.com>
2021-03-15 23:15:30 +0530

    fetching media files while mounting

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -17,6 +17,60 @@ import Input from "@material-ui/core/Input";
 import Button from "@material-ui/core/Button";
 import profileImg from "../../assets/images/upgrad.svg";
 
+const useStyles = (theme) => ({
+    media: {
+      height: 150,
+      paddingTop: "56.25%", // 16:9,
+    },
+  });
+  
+  class Home extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+          profilePic: profileImg,
+          endpoint1: [],
+          postListForSearch: [],
+          postList: [],
+          likeIcon: "dispBlock",
+          likedIcon: "dispNone",
+          comment: "",
+        };
+      }
+    
+      // Invoking APIs when component mounts
+      componentDidMount() {
+        let data = null;
+        let xhr = new XMLHttpRequest();
+        let that = this;
+        let accessToken = window.sessionStorage.getItem("access-token");
+        xhr.addEventListener("readystatechange", function () {
+          if (this.readyState === 4) {
+            that.setState({
+              endpoint1: JSON.parse(this.responseText).data,
+            });
+            //Calling 2nd API only if we get response from 1st API
+            that.state.endpoint1 &&
+              that.state.endpoint1.map((info) => {
+                return that.getImages(info);
+              });
+          }
+        });
+    
+        // https://graph.instagram.com/me/media?fields=id,caption&access_token=YourAccessToken
+    
+        xhr.open(
+          "GET",
+          this.props.baseUrl +
+            "me/media?fields=id,caption&access_token=" +
+            accessToken
+        );
+        xhr.setRequestHeader("Cache-Control", "no-cache");
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(data);
+      }
+    
 
 render() {
     const { classes } = this.props;
@@ -25,7 +79,8 @@ render() {
 
 </div>
     );
-  }
+}
+  
 }
 
 export default withStyles(useStyles)(Home);
