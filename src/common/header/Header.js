7e4912ebde7367ba27e741604b0e07e551d73b10
ChethanShetty563 <chethan563@gmail.com>
2021-03-15 21:39:33 +0530

    Added the logo text

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -6,6 +6,15 @@ class Header extends Component {
     render() {
         return (
             <div>
+                <header className="app-header">
+                <span
+            className="logo"
+            style={this.props.profile === "true" ? { cursor: "pointer" } : null}
+            onClick={this.props.profile === "true" ? this.logoHandler : null}
+          >
+            Image Viewer
+          </span>
+                </header>
                 </div>
         )
     }
