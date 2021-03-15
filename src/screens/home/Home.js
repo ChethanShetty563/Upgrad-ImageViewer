2a4d3818d4b750955a4ea8b9fc44a98a77e315cc
ChethanShetty563 <chethan563@gmail.com>
2021-03-15 23:16:50 +0530

    displaying the images

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -70,12 +70,181 @@ const useStyles = (theme) => ({
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.send(data);
       }
+
+      getImages(info) {
+        let data = null;
+        let xhr = new XMLHttpRequest();
+        let that = this;
+        let accessToken = window.sessionStorage.getItem("access-token");
+        xhr.addEventListener("readystatechange", function () {
+          if (this.readyState === 4) {
+            let parsedData = JSON.parse(this.responseText);
+            let newStateArray;
+            let post = {};
+            post.id = parsedData.id;
+            post.caption = info.caption || "This is default caption";
+            post.media_url = parsedData.media_url;
+            post.profilePic = that.state.profilePic;
+            post.username = parsedData.username;
+            post.likeIcon = "dispBlock";
+            post.likedIcon = "dispNone";
+            post.likesCount = Math.floor(Math.random() * 10);
+            post.clear = "";
+            post.tags = post.caption.match(/#\S+/g);
+            post.commentContent = [];
+            post.timestamp = new Date(parsedData.timestamp);
+            newStateArray = that.state.postList.slice();
+            newStateArray.push(post);
+            that.setState({
+              postList: newStateArray,
+              postListForSearch: newStateArray,
+            });
+          }
+        });
+    
+        //graph.instagram.com/17895695668004550?fields=id,media_type,media_url,username,timestamp&access_token=YourAccessToken
+        xhr.open(
+          "GET",
+          this.props.baseUrl +
+            info.id +
+            "?fields=id,media_type,media_url,username,timestamp&access_token=" +
+            accessToken
+        );
+        xhr.setRequestHeader("Cache-Control", "no-cache");
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(data);
+      }
+    
+      // function for displaying the filtered post
+      filteredPostHandler = (filteredPost) => {
+        this.setState({ postList: filteredPost });
+      };
     
 
 render() {
     const { classes } = this.props;
     return (
       <div>
+           {/* display the contents only if the user is logged in */}
+        {sessionStorage.getItem("access-token") !== null ? (
+          <div>
+            <Header
+              home="true"
+              profilePic={this.state.profilePic}
+              baseUrl={this.props.baseUrl}
+              list={this.state.postListForSearch}
+              callbackFromHome={this.filteredPostHandler}
+              history={this.props.history}
+            />
+            <div className="container">
+              {this.state.postList.map((post) => (
+                <Card className="cards-layout" key={"post" + post.id}>
+                  <div className="posts">
+                    <CardHeader
+                      avatar={<Avatar src={post.profilePic} alt="pic" />}
+                      title={post.username}
+                      // subheader="03/10/2018 16:07:24"
+                      subheader={this.getPostDate(post.timestamp)}
+                    />
+                    <CardContent>
+                      <CardMedia
+                        className={classes.media}
+                        image={post.media_url}
+                      />
+                      <hr />
+                      <Typography variant="body2" color="inherit" component="p">
+                        {post.caption}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        style={{ color: "blue" }}
+                        display="inline"
+                      >
+                        {post.tags &&
+                          post.tags.map((value, key) => {
+                            return (
+                              <span
+                                key={"tag" + key}
+                                style={{ marginRight: 5 }}
+                              >
+                                {" "}
+                                {value}{" "}
+                              </span>
+                            );
+                          })}
+                      </Typography>
+                      <CardActions disableSpacing>
+                        <div className="likes">
+                          <div
+                            className={post.likeIcon}
+                            onClick={() => this.likeClickHandler(post.id)}
+                          >
+                            <FavoriteBorderIcon />
+                          </div>
+                          <div className={post.likedIcon}>
+                            <FavoriteIcon
+                              style={{ color: "red" }}
+                              onClick={() => this.likedClickHandler(post.id)}
+                            />
+                          </div>
+                          <span style={{ marginLeft: 10, marginBottom: 8 }}>
+                            {post.likesCount < 2 ? (
+                              <div> {post.likesCount} like </div>
+                            ) : (
+                              <div> {post.likesCount} likes </div>
+                            )}
+                          </span>
+                        </div>
+                      </CardActions>
+                      <div className="comments-section">
+                        {post.commentContent.map((value, key) => (
+                          <CardActions>
+                            <div key={"comment" + key}>
+                              <Typography
+                                variant="body2"
+                                color="inherit"
+                                component="span"
+                                style={{ fontWeight: "bold" }}
+                              >
+                                {post.username}:{" "}
+                              </Typography>
+                              {value}
+                            </div>
+                          </CardActions>
+                        ))}
+                      </div>
+                      <br />
+                      <div className="comments">
+                        <FormControl className="control">
+                          <InputLabel htmlFor="comment">
+                            Add a comment
+                          </InputLabel>
+                          <Input
+                            comment={this.state.comment}
+                            onChange={(e) =>
+                              this.commentChangeHandler(e, post.id)
+                            }
+                            value={post.clear}
+                          />
+                        </FormControl>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          style={{ marginLeft: 20 }}
+                          onClick={() => this.addCommentHandler(post.id)}
+                        >
+                          ADD
+                        </Button>
+                      </div>
+                    </CardContent>
+                  </div>
+                </Card>
+              ))}
+            </div>
+          </div>
+        ) : (
+          this.props.history.push("/")
+        )}
 
 </div>
     );
