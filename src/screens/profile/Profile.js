e4733cf15ee0498fd4604fc77d7cc64fb149a7b6
ChethanShetty563 <chethan563@gmail.com>
2021-03-15 23:28:56 +0530

    Added the profile class component

---
+++ src/screens/profile/Profile.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Header from "../../common/header/Header";
+import profileImg from "../../assets/images/upgrad.svg";
+import "./Profile.css";
+import Typography from "@material-ui/core/Typography";
+import Fab from "@material-ui/core/Fab";
+import EditIcon from "@material-ui/icons/Edit";
+import Modal from "@material-ui/core/Modal";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import Avatar from "@material-ui/core/Avatar";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+
+
+class Profile extends Component {
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div>
+
+            </div>
+
+);
+}
+}
+
+export default withStyles(styles)(Profile);
