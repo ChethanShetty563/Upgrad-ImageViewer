4cca16d043bfb5796b4151cf40f328e76d11d3c2
ChethanShetty563 <chethan563@gmail.com>
2021-03-15 23:12:15 +0530

    Created Home class component

---
+++ src/screens/home/Home.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import Header from "../../common/header/Header";
+import "./Home.css";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Button from "@material-ui/core/Button";
+import profileImg from "../../assets/images/upgrad.svg";
+
+
+render() {
+    const { classes } = this.props;
+    return (
+      <div>
+
+</div>
+    );
+  }
+}
+
+export default withStyles(useStyles)(Home);
