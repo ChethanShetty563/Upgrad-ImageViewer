0b242813e7fffb27fd35173f9c49e6fee2432597
ChethanShetty563 <chethan563@gmail.com>
2021-03-15 23:30:56 +0530

    fetching the images while mounting

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -29,7 +29,97 @@ function getModalStyle() {
     };
   }
 
+
+  const styles = (theme) => ({
+    paper: {
+      position: "absolute",
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: "20px",
+      outline: "none",
+      borderRadius: "5px",
+      borderColor: "gray",
+      borderStyle: "solid",
+    },
+  
+    root: {
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "space-around",
+      overflow: "hidden",
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      width: "95%",
+      height: 750,
+      cursor: "pointer",
+      overflow: "hidden",
+    },
+  });
+  
 class Profile extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+          profilePic: profileImg,
+          endpoint1: [],
+          username: "",
+          totalPostCount: 0,
+          NumOfUsersFollowed: Math.floor(Math.random() * 100),
+          NumOfFollowers: Math.floor(Math.random() * 100),
+          fullName: "Chethan Shetty",
+          editModal: false,
+          nameRequired: "dispNone",
+          name: "",
+          postList: [],
+          postModal: false,
+          imageUrl: "",
+          caption: "",
+          tags: [],
+          postComments: [],
+          likeIcon: "",
+          likedIcon: "",
+          likesCount: 0,
+          postId: 0,
+          comment: "",
+        };
+      }
+    
+      // Invoking APIs when component mounts
+      componentDidMount() {
+        let data = null;
+        let xhr = new XMLHttpRequest();
+        let that = this;
+        let accessToken = window.sessionStorage.getItem("access-token");
+        xhr.addEventListener("readystatechange", function () {
+          if (this.readyState === 4) {
+            that.setState({
+              endpoint1: JSON.parse(this.responseText).data,
+            });
+            that.setState({
+              totalPostCount: that.state.endpoint1 && that.state.endpoint1.length,
+            });
+            //Calling 2nd API only if we get response from 1st API
+            that.state.endpoint1 &&
+              that.state.endpoint1.map((info) => {
+                return that.getImages(info);
+              });
+          }
+        });
+    
+        // https://graph.instagram.com/me/media?fields=id,caption&access_token=YourAccessToken
+    
+        xhr.open(
+          "GET",
+          this.props.baseUrl +
+            "me/media?fields=id,caption&access_token=" +
+            accessToken
+        );
+        xhr.setRequestHeader("Cache-Control", "no-cache");
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(data);
+      }
 
     render() {
         const { classes } = this.props;
